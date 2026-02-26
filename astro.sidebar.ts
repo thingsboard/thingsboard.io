a3689f53fa4f900f445e2b8e973986318d3b46b3
Yevheniia Mala <ymala@thingsboard.io>
2026-02-26 13:10:21 +0200

    alarms docs

--- astro.sidebar.ts
+++ astro.sidebar.ts
@@ -58,6 +58,7 @@ const guideItems = (prefix: string) => [
 			`${prefix}/alarms`,
 			`${prefix}/alarm-rules`,
 			`${prefix}/notifications`,
+			`${prefix}/notification-template-parameters`,
 		],
 	},
 	{
@@ -170,6 +171,13 @@ const recipeItems = (prefix: string) => [
 		collapsed: true,
 		items: [
 			`${prefix}/configure-telemetry-ttl`,
+		],
+	},
+	{
+		label: 'Alarms',
+		collapsed: true,
+		items: [
+			`${prefix}/alarm-rule-tutorials`,
 		],
 	},
 ];
