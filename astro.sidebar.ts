1aeee767b82af30bc4e589b25e4b43758a086cde
Roman Kostenko <romen.kost.2332@gmail.com>
2026-03-10 12:06:55 +0200

    Migrate cache settings

--- astro.sidebar.ts
+++ astro.sidebar.ts
@@ -883,6 +883,7 @@ export const trendzSidebar: SidebarConfig = [
 			},
 			'docs/trendz/mix-sql-datasource',
 			'docs/trendz/states',
+			'docs/trendz/cache-settings',
 			'docs/trendz/settings',
 			'docs/trendz/tasks-service',
 		],
