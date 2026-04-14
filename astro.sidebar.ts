1f9d3be7dade8ee346aacff3402ce840cbb32782
stitenko <stitenko@thingsboard.io>
2026-04-14 12:27:05 +0300

    Enrich Alarms with Details

--- astro.sidebar.ts
+++ astro.sidebar.ts
@@ -339,7 +339,7 @@ const recipeItems = (prefix: string, extraProcessingItems: string[] = []) => [
 	{
 		label: 'Processing Data',
 		collapsed: true,
-		items: [`${prefix}/python-telemetry`, `${prefix}/telemetry-delta-two-devices`, `${prefix}/trigger-related-entities-via-relation`, ...extraProcessingItems],
+		items: [`${prefix}/python-telemetry`, `${prefix}/telemetry-delta-two-devices`, `${prefix}/trigger-related-entities-via-relation`, `${prefix}/enrich-alarms-with-details`, ...extraProcessingItems],
 	},
 	{
 		label: 'Validating Data',
